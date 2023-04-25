4933f53b2b7054c20d0e54382ccbe8dbe457c5e7
elishaJ <elisha.jamil@gmail.com>
2023-04-25 15:24:18 +0500

    Set up task7

--- task7/main.tf
+++ task7/main.tf
@@ -1,26 +1,9 @@
-# Create bucket
-resource "aws_s3_bucket" "bucket-for-git-actions" {
-  bucket = var.bucket
-  force_destroy = true
-}
-
-# Resource to block all public access to bucket
-resource "aws_s3_bucket_public_access_block" "block-bucket-public-access" {
-  bucket = aws_s3_bucket.bucket-for-git-actions.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.bucket-for-git-actions.id
-  versioning_configuration {
-    status = "Enabled"
+module "bucket-creation" {
+  source = "./modules/s3creation"
+  providers = {
+    aws = aws.Oregon
   }
 }
-
 output "bucket-arn" {
-        value = aws_s3_bucket.bucket-for-shared-access.arn
+  value = module.bucket-creation.bucket-arn
 }
--- task7/providers.tf
+++ task7/providers.tf
@@ -1,12 +1,23 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "4.61.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "tf-state-bk-007"
+    key            = "state/task7/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-lock-table-1292"
+    encrypt        = true
+    profile        = "default"
   }
 }
 
 provider "aws" {
-  # Configuration options
+  alias  = "Oregon"
+  region = "us-west-2"
 }
+

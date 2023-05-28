25651fe1cf75f222a6c8f9ab2cf054f69d22cb01
elishaJ <elisha.jamil@gmail.com>
2023-05-28 15:44:49 +0500

    Fix permissions for git action

--- task7/main.tf
+++ task7/main.tf
@@ -14,6 +14,7 @@ resource "aws_s3_bucket_public_access_block" "block-bucket-public-access" {
   restrict_public_buckets = true
 }
 
+# Enable bucket versioning
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.my-bucket.id
   versioning_configuration {

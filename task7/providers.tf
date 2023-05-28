aaa4a6ee152558868bfb91cb6c15e13afef399ad
elishaJ <elisha.jamil@gmail.com>
2023-05-28 15:58:18 +0500

    Edit backend for task7

--- task7/providers.tf
+++ task7/providers.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket         = "tf-state-bk-007"
-    key            = "state/task7/terraform.tfstate"
+    key            = "state/t7/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tf-lock-table-1292"
     encrypt        = true
